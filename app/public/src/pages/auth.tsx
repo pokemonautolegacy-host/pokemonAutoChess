73e4c5cf3178e16dee17ad9591f69b3723023a13
PokemonAutoLegacy <pokemonautolegacy@gmail.com>
2026-06-17 16:52:20 -0600

    Revert change to logo
    It's going to be better to just change the base file names instead of hunting for uses

--- app/public/src/pages/auth.tsx
+++ app/public/src/pages/auth.tsx
@@ -27,7 +27,7 @@ export default function Auth() {
       {isSupposedlyMobile && (
         <p className="mobile-warning">{t("mobile_warning")}</p>
       )}
-      <img className="logo" src="assets/ui/pal_logo.svg" />
+      <img className="logo" src="assets/ui/pokemon_autochess_final.svg" />
       <header>
         <h1>{t("pokemon_auto_chess")}</h1>
         <div className="disclaimer">
